270a2f83ca9dcc3600addec27057feb182f2aadd
sunimar <43272283+sunimar@users.noreply.github.com>
2019-05-09 19:10:46 -0300

    Add files via upload

--- src/service/FichaMedicaService.java
+++ src/service/FichaMedicaService.java
@@ -1,6 +1,7 @@
 package service;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 import org.simpleframework.http.Query;
 import org.simpleframework.http.Request;
@@ -10,6 +11,7 @@ import dao.JogadorDAO;
 import model.FichaMedica;
 import model.Jogador;
 import model.enums.Escala;
+import model.enums.NivelLesao;
 
 public class FichaMedicaService {
 
@@ -29,12 +31,32 @@ public class FichaMedicaService {
 	
 	public String listarFichasMedicas(Request request) {
 		try {
-			return fichaDAO.toString();//provavel de estar errado -> return listaJogadoresJSON().toString();
+			return fichaDAO.toJSON().toString();//provavel de estar errado -> return listaJogadoresJSON().toString();
 		} catch (Exception e) {
 			e.printStackTrace();
 			return "Erro ao consultar Ficha Medica";
 		}
 	}//listar as fichas
+	
+	public String adicionarFichaMedica(Request request) {
+		Query query = request.getQuery();
+
+		try {
+			
+			FichaMedica f = new FichaMedica();
+		
+			f.setJogador(jogadorDAO.get(Integer.parseInt(query.get("id"))));
+			f.setNivelDaLesao(NivelLesao.findByValor(Integer.parseInt(query.get("nivel"))));
+			f.setDataEntrada(LocalDateTime.parse());//converter texto para data
+					
+			fichaDAO.add(f);
+
+			return listaJSON().toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "Erro ao adicionar Jogador";
+		}
+	}
 	
 	/*
 	 * Inserir ficha, deve comunicar com inserir jogador também?
