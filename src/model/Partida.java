9c8e627c97bbad1e3c68c3ff305b10c8f304a363
michelOlivc <41274152+michelOlivc@users.noreply.github.com>
2019-05-25 11:01:24 -0300

    Add files via upload

--- src/model/Partida.java
+++ src/model/Partida.java
@@ -1,21 +1,63 @@
 package model;
+
 import java.util.ArrayList;
+import java.util.List;
+import org.json.JSONArray;
 
+import org.json.JSONObject;
 
+public class Partida implements JsonFormatter {
+	private Integer id;
 
-public class Partida {
-int id;	
+	private List<Estatistica> estatisticasJogador;
 
-List<Estatistica> estatisticasJogador; 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public Partida() {
+		this.estatisticasJogador = new ArrayList<Estatistica>();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public List<Estatistica> getEstatisticasJogador() {
+		return estatisticasJogador;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public void setEstatisticasJogador(List<Estatistica> estatisticasJogador) {
+		this.estatisticasJogador = estatisticasJogador;
+	}
+
+	public void inserirEstatistica(Estatistica estatistica) {
+
+		if (estatisticasJogador.size() < 18 && estatistica != null) {
+			estatisticasJogador.add(estatistica);
+		}
+
+	}
+
+	@Override
+	public JSONObject toJson() {
+		JSONObject obj = new JSONObject();
+		obj.put("id", this.id);
+		obj.put("estatisticas", this.estatisticasJogador.toArray());
+		return obj;
+	}
+
+	@Override // exemplo de time
+	public JSONArray toJsonArray() {
+		JSONArray array = new JSONArray();
+		for (Estatistica j : this.estatisticasJogador) {
+			array.put(j.toJson());
+		}
+		return array;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return this.id == ((Partida) obj).id;
+	}
 }
